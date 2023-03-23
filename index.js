f2cc74059620df1ccc5791f6a2c4b8bf57a8d441
Andrew <armartin1998@gmail.com>
2023-03-23 07:28:48 -0400

    Complete has target sum lab

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for (let i = 0; i < array.length; i++){
+    for (let j = i + 1; j < array.length; j++){
+      if (array[i] + array[j] === target) return true
+    } 
+  }
+  return false
 }
 
 /* 
   Write the Big O time complexity of your function here
+  0(n^2)
 */
 
 /* 
   Add your pseudocode here
+  function takes an array and target number
+    checks the first number against the rest of the numbers in the array
+      if the pair adds up to target return true
+      if not return false
 */
 
 /*
   Add written explanation of your solution here
+  function will grab the starting number in the array and loop through the rest
+  of it checking to see if any pair add up to target. this will continue until the 
+  end of the array. if a pair adds up to the target it returns true if not false
 */
 
 // You can run `node index.js` to view these console logs
@@ -29,6 +43,11 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+  console.log("")
+
+  console.log(hasTargetSum([1,1], 2))
+  console.log(hasTargetSum([1, 3, 7, 9], 10))
 }
 
 module.exports = hasTargetSum;
